924930ad9ab0557d6bcf1a99f3d43cf99269fc21
Choi TaeSan <xotks7524@gmail.com>
2023-12-12 23:35:19 +0900

    Refactor: 최적화

--- src/main/java/com/example/todo_timer/TodoTimerController.java
+++ src/main/java/com/example/todo_timer/TodoTimerController.java
@@ -7,7 +7,6 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
@@ -16,8 +15,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
-
-import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
