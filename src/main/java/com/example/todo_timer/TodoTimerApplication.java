55f9bec0cb659914b85e4162d943e5836b5160c6
Choi TaeSan <xotks7524@gmail.com>
2023-12-13 00:45:28 +0900

    Feat: 카테고리 씬 추가 - 카테고리 씬 → 메인 씬

--- src/main/java/com/example/todo_timer/TodoTimerApplication.java
+++ src/main/java/com/example/todo_timer/TodoTimerApplication.java
@@ -17,7 +17,7 @@ public class TodoTimerApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
         //Objects.requireNonNull은 getResource("TodoTimer.fxml")의 반환값이 null이면 예외를 발생시키므로,
         // 리소스가 없는 경우에는 예외가 발생
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TodoTask.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TodoMain.fxml")));
         primaryStage.setTitle("ToDo 타이머");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
